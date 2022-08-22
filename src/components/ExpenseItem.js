6f2416e7a2564d63a4f9c551bd84e3cbbca69c40
Juma-h <mitchfibi@gmail.com>
2022-08-22 10:45:55 +0300

    changed the date object to string and passed date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-
+      {/* passed the date as a property */}
+      <ExpenseDate date={props.date}/> 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">KES. {props.amount}</div>
